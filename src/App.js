ba2cee953bcb0b42adcae8b7bbab6bbff579360f
Jesus Lancianese <jesuwlan@gmail.com>
2023-04-07 16:58:33 -0400

    Changes to final project

--- src/App.js
+++ src/App.js
@@ -6,10 +6,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Budget from './components/Budget';
 import Remaining from './components/Remaining';
 import ExpenseTotal from './components/ExpenseTotal';
-import ExpenseItem from './components/ExpenseItem';
 import ExpenseList from './components/ExpenseList';
 import AllocationForm from './components/AllocationForm';
-
+import Currency from './components/Currency';
 
 // Add code to import the other components here under
 
@@ -29,6 +28,9 @@ const App = () => {
                     </div>
                     <div className='col-sm'>
                         <ExpenseTotal />
+                    </div>
+                    <div className='col-sm'>
+                        <Currency />
                     </div>
                 </div>
                 <h3 className='mt-3'>Allocation</h3>
